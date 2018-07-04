3da721622819a4248823160728842dac382218bb
adam slack <adamslack@outlook.com>
2018-07-04 13:02:23 +0100

    initial bar in there now

--- app/src/main/java/org/sociam/koalahero/additionalInfoActivities/AdditionalInfoTrackersActivity.java
+++ app/src/main/java/org/sociam/koalahero/additionalInfoActivities/AdditionalInfoTrackersActivity.java
@@ -9,6 +9,13 @@ import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
 import org.sociam.koalahero.R;
 import org.sociam.koalahero.appsInspector.App;
 import org.sociam.koalahero.appsInspector.AppModel;
@@ -21,6 +28,7 @@ import org.w3c.dom.Text;
 
 import java.io.Console;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import org.sociam.koalahero.audio.AudioRecorder;
 
@@ -75,6 +83,18 @@ public class AdditionalInfoTrackersActivity extends AppCompatActivity {
         genreAvgHostCount.setText(String.valueOf((int) thisAppsGenreInfo.genreAvgHosts));
         genreAvgHostLabel.setText(thisAppsGenreInfo.getAppGenre().toLabel());
 
+
+        ArrayList<Integer> barValues = new ArrayList<>();
+        barValues.add(hostNames.size());
+        barValues.add((int) thisAppsGenreInfo.genreAvgHosts);
+        barValues.add(0);
+
+        ArrayList<String> axisLabels = new ArrayList<String>(Arrays.asList("This App", "Genre Avg", "All Apps Avg"));
+
+        BarData bd = this.buildBarData(barValues);
+        this.buildHostBarChart(bd ,axisLabels);
+
+
         // Set information read from device
         try {
             ApplicationInfo appInfo = getPackageManager().getApplicationInfo(this.selectedApp.getxRayAppInfo().app,0);
@@ -86,5 +106,32 @@ public class AdditionalInfoTrackersActivity extends AppCompatActivity {
         catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    private void buildHostBarChart(BarData barData, ArrayList<String> axisValues){
+        BarChart barChart = (BarChart) findViewById(R.id.hostBarChart);
+        barChart.setData(barData);
+        barChart.setFitBars(true);
+        barChart.getXAxis().setDrawLabels(false);
+        barChart.getXAxis().setDrawAxisLine(false);
+        barChart.getXAxis().setDrawGridLines(false);
+        barChart.getXAxis().setLabelCount(barData.getEntryCount());
+        barChart.setScaleEnabled(false);
+
+        barChart.invalidate();
+    }
+
+    private BarData buildBarData(ArrayList<Integer> data) {
+        ArrayList<BarEntry> barEntries = new ArrayList<BarEntry>();
+        for(Integer point : data) {
+            barEntries.add(new BarEntry(barEntries.size(), point));
+        }
+        BarDataSet bds = new BarDataSet(barEntries, "Host Counts");
+        bds.setColors(ColorTemplate.JOYFUL_COLORS);
+
+        BarData bd = new BarData(bds);
+        bd.setBarWidth(0.9f);
+
+        return bd;
     }
 }
